a95c2b575ac3be8eca20fa23087220fe3b1dddc2
Josipa Mrša <jmrsa@pmfst.hr>
2021-02-27 18:37:15 +0100

    frameguard against clickjacking

--- myApp.js
+++ myApp.js
@@ -1,8 +1,21 @@
 var express = require('express');
 var helmet = require('helmet');
 var app = express();
+app.use(express.json());
 
+// hidePoweredBy() - hide header letting everyone know 
+// the app was made by using Express 
 app.use(helmet.hidePoweredBy());
+
+// frameguard() - against clickjacking (sets x-frame-options header),
+// typical clickjacking attack includes setting an invisible Iframe or
+// frame over visible objects, which contains objects from another website
+
+// For example, clicking a button on the website doesn't do what you think
+// it does (submits some data), but instead there is an invisible button over
+// it that likes someone's Facebook post
+app.use(helmet.frameguard({ action: 'deny '}));
+
 
 
 
