c336491588c74ba13c6317797ccefa0513685411
Josipa Mrša <jmrsa@pmfst.hr>
2021-02-27 17:44:10 +0100

    powered by

--- myApp.js
+++ myApp.js
@@ -2,8 +2,7 @@ var express = require('express');
 var helmet = require('helmet');
 var app = express();
 
-app.use(helmet);
-app.use(helmet.hidePoweredBy({ setTo: 'PHP 4.2.0' }));
+app.use(helmet.hidePoweredBy());
 
 
 
